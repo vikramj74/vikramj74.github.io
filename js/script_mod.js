e666953e4b94163f3c57ae0f1a8cc953bc5fd164
Vikram Jaswal <vikramj74@gmail.com>
2016-08-15 16:57:30 +0530

    JS : comments removed and indentation fixes

--- js/script_mod.js
+++ js/script_mod.js
@@ -3,93 +3,62 @@ function getDocHeight() {
 	return height;
 }
 
-/*function maxify() {
-    var docHeight = getDocHeight();
-    $(".page").css("height",docHeight+"px");
-    var save = docHeight;
-    docHeight = 1.35 *docHeight;
-    $(".page").animate({height:docHeight},20);
-    $("#page3").animate({height:save},20);
-    $("#box").scrollTop(0);
-    $("#brand-link").trigger("click");
-}*/ 
 function goToPageOne() {
     var p = $("#page1").position().top;
-    /*$(document).scrollTop(p);*/
     var html = $('html,body');
     html.animate({scrollTop:p}, 750, 'swing', function() { console.log("Finished animating"); }) ; 
 }
+
 function goToPageTwo() {
     var p = $("#page2").position().top;
-    /*$(document).scrollTop(p);*/
     var html = $('html,body');
     html.animate({scrollTop:p}, 750, 'swing', function() { console.log("Finished animating"); }) ; 
 }
 
 function goToPageThree() {
     var p = $("#page3").position().top;
-    /*$(document).scrollTop(p);*/
     var html = $('html,body');
     html.animate({scrollTop:p}, 750, 'swing', function() { console.log("Finished animating"); }) ; 
 }
+
 $(document).ready(
     function() {
-        //maxify();
-        /* DEBUG BEGIN *\/
-        $('#page1').click(
-        	function() {
-        		var p = $(this).position();
-        		console.log("Page 1 is at : \n\t"+p.top+"px");
-        	}
-        );
-        $('#page2').click(
-        	function() {
-        		var p = $(this).position();
-        		console.log("Page 2 is at : \n\t"+p.top+"px");
-        	}
-        );
-        /* DEBUG END */ 
-        
+                
         $('.go-to-p1').click(goToPageOne);
         $('.go-to-p2').click(goToPageTwo);
         $('.go-to-p3').click(goToPageThree);
             
     
-    
-        /* Parallax script */
-        //$(document).on("mousewheel DOMMouseScroll MozMousePixelScrolling", function(event, data) {
-        /*$(window).scroll(function() { 
-                console.log("It fucking works !!!! Yessssssssssss");
-                var pageYOffset = window.pageYOffset;
-                $("#page2").css("top", -pageYOffset*0.4 + "px");
-            }
-        );*/
         $("#contact-btn").mouseenter(
             function() {
                 $("#contact-btn").toggleClass("focus");
             }
         );
+
         $("#contact-btn").mouseleave(
             function() {
                 $("#contact-btn").toggleClass("focus");
             }
         );
+
         $("#projects-btn").mouseenter(
             function() {
                 $("#projects-btn").toggleClass("focus");
             }
         );
+
         $("#projects-btn").mouseleave(
             function() {
                 $("#projects-btn").toggleClass("focus");
             }
         );
-         $(function(){ 
-             var navMain = $("#navi");
-             navMain.on("click", "li", null, function () {
-                if($(document).width() < 768)
-                    $("#hamburger").trigger('click');
-             });
-         });
+
+        $(function(){ 
+            var navMain = $("#navi");
+            navMain.on("click", "li", null, function () {
+            if($(document).width() < 768)
+                $("#hamburger").trigger('click');
+            });
+        });
     }
 );
